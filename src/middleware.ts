68584c36dd61ae4fe4227e0511806838015d4bb5
sangjun231 <44526444+sangjun231@users.noreply.github.com>
2025-10-17 23:25:37 +0900

    update: 미들웨어 api 조건 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,6 +14,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to fit your needs.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
